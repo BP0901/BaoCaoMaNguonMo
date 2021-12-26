97d2104089519994a1c1ff55a5ffccd66465ab12
TranBaoPhuc <phuc423vn@gmail.com>
2021-12-26 21:00:52 +0700

    [APP]:[Staff]: Hoàn thành giao điện và chức năng phần nhân viên

--- DoAnTinHoc/app/src/main/java/com/stu/doantinhoc/Util/Url.java
+++ DoAnTinHoc/app/src/main/java/com/stu/doantinhoc/Util/Url.java
@@ -2,24 +2,24 @@ package com.stu.doantinhoc.Util;
 
 public class Url {
     //Upload
-    public static final String URLUPLOADCATEPIC = "http://192.168.1.9/androidDACN/UploadImg.php";
+    public static final String URLLOADIMG = "http://192.168.1.11/androidDACN/images/";
 
     //Food
-    public static final String URLGETALLFOOD = "http://192.168.1.9/androidDACN/FoodAPI.php?action=getall";
-    public static final String URLINSERTFOOD = "http://192.168.1.9/androidDACN/FoodAPI.php?action=insert";
-    public static final String URLDELETEFOOD = "http://192.168.1.9/androidDACN/FoodAPI.php?action=delete";
-    public static final String URLUPDATEFOOD = "http://192.168.1.9/androidDACN/FoodAPI.php?action=update";
+    public static final String URLGETALLFOOD = "http://192.168.1.11/androidDACN/FoodAPI.php?action=getall";
+    public static final String URLINSERTFOOD = "http://192.168.1.11/androidDACN/FoodAPI.php?action=insert";
+    public static final String URLDELETEFOOD = "http://192.168.1.11/androidDACN/FoodAPI.php?action=delete";
+    public static final String URLUPDATEFOOD = "http://192.168.1.11/androidDACN/FoodAPI.php?action=update";
 
     //Category
-    public static final String URLGETALLCATE = "http://192.168.1.9/androidDACN/CategoryAPI.php?action=getall";
-    public static final String URLINSERTCATE = "http://192.168.1.9/androidDACN/CategoryAPI.php?action=insert";
-    public static final String URLDELETECATE = "http://192.168.1.9/androidDACN/CategoryAPI.php?action=delete";
-    public static final String URLUPDATECATE = "http://192.168.1.9/androidDACN/CategoryAPI.php?action=update";
+    public static final String URLGETALLCATE = "http://192.168.1.11/androidDACN/CategoryAPI.php?action=getall";
+    public static final String URLINSERTCATE = "http://192.168.1.11/androidDACN/CategoryAPI.php?action=insert";
+    public static final String URLDELETECATE = "http://192.168.1.11/androidDACN/CategoryAPI.php?action=delete";
+    public static final String URLUPDATECATE = "http://192.168.1.11/androidDACN/CategoryAPI.php?action=update";
 
     //User
-    public static final String URLGETALLUSER = "http://192.168.1.9/androidDACN/UserAPI.php?action=getall";
-    public static final String URLINSERTUSER = "http://192.168.1.9/androidDACN/UserAPI.php?action=insert";
-    public static final String URLDELETEUSER = "http://192.168.1.9/androidDACN/UserAPI.php?action=delete";
-    public static final String URLUPDATEUSER = "http://192.168.1.9/androidDACN/UserAPI.php?action=update";
+    public static final String URLGETALLUSER = "http://192.168.1.11/androidDACN/UserAPI.php?action=getall";
+    public static final String URLINSERTUSER = "http://192.168.1.11/androidDACN/UserAPI.php?action=insert";
+    public static final String URLDELETEUSER = "http://192.168.1.11/androidDACN/UserAPI.php?action=delete";
+    public static final String URLUPDATEUSER = "http://192.168.1.11/androidDACN/UserAPI.php?action=update";
 
 }
