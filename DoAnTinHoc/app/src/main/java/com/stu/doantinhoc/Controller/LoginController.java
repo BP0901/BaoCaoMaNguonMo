9f6ed2552ba25142488c2492e5427c71aebd0472
TranBaoPhuc <phuc423vn@gmail.com>
2021-12-26 23:40:39 +0700

    Hoàn thành chương trình

--- DoAnTinHoc/app/src/main/java/com/stu/doantinhoc/Controller/LoginController.java
+++ DoAnTinHoc/app/src/main/java/com/stu/doantinhoc/Controller/LoginController.java
@@ -2,14 +2,10 @@ package com.stu.doantinhoc.Controller;
 
 import android.content.Context;
 import android.content.Intent;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.stu.doantinhoc.Activities.Admin.MainMenuActivity;
 import com.stu.doantinhoc.Activities.Components.MsgDialog;
 import com.stu.doantinhoc.Activities.Staff.MainStaffActivity;
-import com.stu.doantinhoc.Model.Category;
-import com.stu.doantinhoc.Model.Food;
 import com.stu.doantinhoc.Model.Table;
 import com.stu.doantinhoc.Model.User;
 import com.stu.doantinhoc.RequestHttp.CategoryRequestHttp;
@@ -45,15 +41,9 @@ public class LoginController {
             if (user.getUsername().equals(username))
                 if (user.getPassword().equals(password))
                     if (user.isType()) {
-<<<<<<< HEAD
-//                        context.startActivity(new Intent(context, MainMenuActivity.class));
-                    } else {
-                        context.startActivity(new Intent(context, MainStaffActivity.class));
-=======
                         context.startActivity(new Intent(context, MainMenuActivity.class));
                     } else {
-//                        context.startActivity(new Intent(context, MainStaffActivity.class));
->>>>>>> 7867c4b3c3f963581a8ab111df661d16e7572fd8
+                        context.startActivity(new Intent(context, MainStaffActivity.class));
                     }
                 else {
                     MsgDialog.showDialog(context, "Sai mật khẩu hoặc tài khoản!");
@@ -67,21 +57,21 @@ public class LoginController {
 
     public void loadDataFromDB() {
 
-            ListData.listCate.clear();
-            CategoryRequestHttp requestHttp = new CategoryRequestHttp(context);
-            requestHttp.getData(Url.URLGETALLCATE);
+        ListData.listCate.clear();
+        CategoryRequestHttp requestHttp = new CategoryRequestHttp(context);
+        requestHttp.getData(Url.URLGETALLCATE);
 
-            ListData.listFood.clear();
-            FoodRequestHttp foodRequestHttp = new FoodRequestHttp(context);
-            foodRequestHttp.getData(Url.URLGETALLFOOD);
+        ListData.listFood.clear();
+        FoodRequestHttp foodRequestHttp = new FoodRequestHttp(context);
+        foodRequestHttp.getData(Url.URLGETALLFOOD);
 
-            if(ListData.FistRun) {
-                for (int i = 1; i <= 15; i++) {
-                    ListData.listTable.add(new Table("Table " + i, new ArrayList<>()));
-                }
-                ListData.FistRun = false;
+        if (ListData.FistRun) {
+            for (int i = 1; i <= 15; i++) {
+                ListData.listTable.add(new Table("Table " + i, new ArrayList<>()));
             }
-
-
+            ListData.FistRun = false;
         }
+
+
+    }
 }
